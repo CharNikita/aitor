e29bb49bf3b12edc142f203d0c3143b3fa0767ad
goncharenko <goncharenkonikitae@gmail.com>
2025-03-02 02:20:31 +0300

    git init

---
+++ src/main/java/ru/goncharenko/aitor/AitorApplication.java
@@ -0,0 +1,11 @@
+package ru.goncharenko.aitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AitorApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/goncharenko/aitor/AitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.goncharenko.aitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
